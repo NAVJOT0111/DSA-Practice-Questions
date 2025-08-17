e432f0fe29e10df3851f79ff76f96e8c72e3ac34
Navjot Kaur <navjotkaur1440@gmail.com>
2025-08-17 10:25:25 +0530

    bfs dfs graphs

---
+++ graphs/matrix.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    // adjacency matrix for undirected graph
+    // time complexity: O(n)
+    int adj[n+1][n+1];
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1;  // this statement will be removed in case of directed graph
+    }
+
+     // adjacency list for undirected graph
+    // time complexity: O(2E)
+    vector<int> adj[n+1];
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    return 0;
+}
+
